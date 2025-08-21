945ea4d3a4a0f4e7b4c293975a8fc26d8b17ac18
freeutka <freeutka@inbox.lv>
2025-08-21 23:53:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,82 +11,54 @@ target_dir=""
 chooseDirectory() {
     echo -e "<Code Editor For Jexactyl> [1] /var/www/jexactyl   (official Jexactyl)"
     echo -e "<Code Editor For Jexactyl> [2] /var/www/pterodactyl (migrated from Pterodactyl)"
+
     while true; do
         read -p "<Code Editor For Jexactyl> [?] Choose directory [1/2]: " choice
         case "$choice" in
             1) target_dir="/var/www/jexactyl"; break ;;
             2) target_dir="/var/www/pterodactyl"; break ;;
-            *) echo -e "${watermark} Invalid choice. Please enter 1 or 2." ;;
+            *) echo -e "\033[0;33m<Code Editor For Jexactyl> \033[0;31m[✕]\033[0m Invalid choice." ;;
         esac
     done
 }
 
-patchWebpack(){
+patchWebpack() {
     config_file="$target_dir/webpack.config.js"
     if ! grep -q "monaco-editor" "$config_file"; then
-        if ! grep -q "require('path')" "$config_file"; then
-            sed -i "1i const path = require('path');" "$config_file"
-        fi
-
-        sed -i '/test: \\/\\.mjs\\$/{n;N;a \
-            {\
-                test: /\.m?js$/, \
-                include: path.resolve(__dirname, '\''node_modules/monaco-editor'\''), \
-                type: '\''javascript/auto'\'', \
-                loader: '\''esbuild-loader'\'', \
-            },' "$config_file"
-
+        awk '1;/test: \\/\.mjs\\$/{print "            {\n                test: /.m?js$/,\n                include: /node_modules\\/monaco-editor/,\n                type: '\''javascript/auto'\'',\n                loader: '\''esbuild-loader'\''\n            }"}' "$config_file" > "$config_file.tmp" && mv "$config_file.tmp" "$config_file"
         printf "${watermark} Patched webpack.config.js with Monaco loader rule \n"
     else
         printf "${watermark} Webpack already patched, skipping \n"
     fi
 }
 
-startPterodactyl(){
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-    nvm install node
-    npm i -g yarn
-    yarn
-    yarn add esbuild-loader monaco-editor @monaco-editor/react
+rebuildAssets() {
+    cd "$target_dir" || { echo "Target directory not found"; exit 1; }
     export NODE_OPTIONS=--openssl-legacy-provider
+    npm install -g yarn
+    yarn install
+    yarn add esbuild-loader monaco-editor @monaco-editor/react
     yarn build:production || { export NODE_OPTIONS=; yarn build:production; }
     php artisan optimize:clear
 }
 
-installModule(){
+installModule() {
     chooseDirectory
     printf "${watermark} Installing module... \n"
-    cd "$target_dir"
-
+    
     patchWebpack
 
-    rm -rvf jexactyl-monaco
+    rm -rf "$target_dir/jexactyl-monaco"
     git clone https://github.com/freeutka/jexactyl-monaco.git
-    rm -f resources/scripts/components/server/files/FileEditContainer.tsx
-    cd jexactyl-monaco
-    mv resources/FileEditContainer.tsx "$target_dir/resources/scripts/components/server/files/"
-    rm -rvf "$target_dir/jexactyl-monaco"
+    rm -f "$target_dir/resources/scripts/components/server/files/FileEditContainer.tsx"
+    mv jexactyl-monaco/resources/FileEditContainer.tsx "$target_dir/resources/scripts/components/server/files/"
+    rm -rf jexactyl-monaco
 
     printf "${watermark} Module fully installed \n"
 
-    while true; do
-        read -p '<Code Editor For Jexactyl> [?] Rebuild panel assets [y/N]? ' yn
-        case $yn in
-            [Yy]* ) startPterodactyl; break;;
-            [Nn]* ) exit;;
-            * ) exit;;
-        esac
-    done
+    read -p '<Code Editor For Jexactyl> [?] Rebuild panel assets [y/N]? ' yn
+    case $yn in [Yy]*) rebuildAssets;; *) exit;; esac
 }
 
-while true; do
-    read -p '<Code Editor For Jexactyl> [?] Are you sure you want to install module [y/N]? ' yn
-    case $yn in
-        [Yy]* ) installModule; break;;
-        [Nn]* ) printf "${watermark} Canceled \n"; exit;;
-        * ) exit;;
-    esac
-done
+read -p '<Code Editor For Jexactyl> [?] Are you sure you want to install module [y/N]? ' yn
+case $yn in [Yy]*) installModule;; *) printf "${watermark} Canceled \n"; exit;; esac
